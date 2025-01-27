525ff5685cf6e6b5df74cd5c4bc89c36adeec8b7
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-27 19:00:18 +0530

    .

---
+++ Collections/Test.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+        ArrayList a1 = new ArrayList<>();
+        a1.add(10);
+        a1.add("karan");
+        a1.add('c');
+        System.out.println(a1);
+
+        HashSet hs = new HashSet<>();
+        hs.add(20);
+        hs.add("Vishal");
+        hs.add('c');
+        System.out.println(hs);
+    }
+}
---
+++ Collections/Test2.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+
+public class Test2 {
+    public static void main(String[] args) {
+        HashMap hm = new HashMap<>();
+
+        hm.put(101, "Karan");
+        hm.put(202, "Vishal");
+        hm.put(303, "kajal");
+        System.out.println(hm);
+    }
+}
