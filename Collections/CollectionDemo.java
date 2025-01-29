0e036103db4096c2fa4a8f803e4e3fcb8bde242f
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-29 08:38:13 +0530

    .

---
+++ Collections/CollectionDemo.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class CollectionDemo {
+    public static void main(String[] args) {
+        ArrayList a1 = new ArrayList<>();
+        a1.add("aaa");
+        a1.add("bbb");
+        a1.add("ccc");
+        System.out.println(a1);
+        System.out.println(a1.size());
+        System.out.println(a1.remove("bbb"));
+        System.out.println(a1);
+        
+
+
+        // ArrayList a2 = new ArrayList<>();
+        // a2.add("aaaa");
+        // a2.add("bbbb");
+        // a2.add("cccc");
+        // System.out.println(a2);
+
+        // a1.addAll(a2);
+        // System.out.println(a1.add(a2));
+        // System.out.println(a1.add("aaaa"));
+        // System.out.println(a2);
+        // System.out.println(a1.contains(100));
+        // System.out.println(a1.isEmpty());
+       
+
+    }
+}
