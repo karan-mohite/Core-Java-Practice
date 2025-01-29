a99f3b22262c559847f5a3dfa7e687297d1068ec
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-29 18:04:26 +0530

    .

---
+++ Collections/ArrayListDemo1.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+
+public class ArrayListDemo1 {
+    public static void main(String[] args) {
+
+        ArrayList a1 = new ArrayList<>();
+
+        a1.add(0, 10);
+        a1.add(1, 100);
+        a1.add(2, 'c');
+        a1.add(3, "Karan");
+        a1.add(4, 50);
+
+        // ArrayList al2 = new ArrayList<>();
+        // al2.add(10);
+        // al2.add("Vishal");
+        // al2.add(50);
+        // al2.removeAll(a1);
+
+        // System.out.println(a1);
+        // a1.clear();
+
+        System.out.println(a1.get(3));
+        System.out.println(a1.size());
+        System.out.println(a1.contains(50));
+    }
+}
