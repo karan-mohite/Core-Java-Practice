8d883084e2051d7654ed62a866af299f873e3c78
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-29 11:19:52 +0530

    .

---
+++ Collections/Test1.java
@@ -0,0 +1,17 @@
+import java.util.Enumeration;
+import java.util.Vector;
+
+public class Test1 {
+    public static void main(String[] args) {
+        Vector v = new Vector<>();
+        v.add(10);
+        v.add("Karan");
+        v.add(15.2);
+        // System.out.println(v);
+
+        Enumeration e = v.elements();
+        while (e.hasMoreElements()) {
+            System.out.println(e.nextElement());
+        }
+    }
+}
