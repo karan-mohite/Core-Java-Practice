9381e8518fe12e1d8bc90b45d4d91a83df416088
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-30 10:23:32 +0530

    .

---
+++ Collections/TreeSetDemo.java
@@ -0,0 +1,24 @@
+import java.util.TreeSet;
+
+public class TreeSetDemo {
+    public static void main(String[] args) {
+        TreeSet ts = new TreeSet<>();
+
+        ts.add(10);
+        ts.add(30);
+        ts.add(40);
+        ts.add(20);
+        ts.add(50);
+        ts.add(60);
+
+        // ts.add("Karan");
+        // ts.add("Amit");
+        // ts.add("Dataa");
+        // ts.add("Kunal");
+
+        // ts.clear();
+        // ts.remove(20);
+
+        System.out.println(ts);
+    }
+}
