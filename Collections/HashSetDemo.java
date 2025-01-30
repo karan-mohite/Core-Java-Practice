01eeaf2a39f59ffd567bc0356adcd6581acd45fe
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-30 09:47:01 +0530

    .

--- Collections/HashSetDemo.java
+++ Collections/HashSetDemo.java
@@ -1,8 +1,17 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 
 public class HashSetDemo {
     public static void main(String[] args) {
+
+        ArrayList al=new ArrayList<>();
+
+        al.add("Karan");
+        al.add(1010);
+        al.add('c');
+
+
         HashSet hs = new HashSet<>();
         // hs.add(10);
         // hs.add("Karan");
@@ -13,17 +22,26 @@ public class HashSetDemo {
         // hs.add(null);
         // hs.add(null);
 
+        hs.addAll(al);
         hs.add(30);
         hs.add(10);
         hs.add(20);
         hs.add(40);        
 
+        // hs.clear();
+
+        hs.remove(30);
+        System.out.println(hs.size());
         System.out.println(hs);
 
-       Iterator itr= hs.iterator();
-       while (itr.hasNext())
-       {
-            System.out.println(itr.next()); 
-       }
+    //    System.out.println(hs.contains("Karan"));
+
+    //     System.out.println(hs);
+
+    //    Iterator itr= hs.iterator();
+    //    while (itr.hasNext())
+    //    {
+    //         System.out.println(itr.next()); 
+    //    }
     }
 }
