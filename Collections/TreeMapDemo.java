b84b444f1b627d020513d76b561ffe81dc0c7c02
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-30 14:51:38 +0530

    .

---
+++ Collections/TreeMapDemo.java
@@ -0,0 +1,51 @@
+import java.util.TreeMap;
+
+public class TreeMapDemo {
+    public static void main(String[] args) {
+        TreeMap map = new TreeMap<>();
+
+        map.put(106, "Karan");
+        map.put(102, "Vishal");
+        map.put(105, "Kamale");
+        map.put(108, "Kajal");
+        map.put(104, "Rahul");
+
+        // map.put("Karan", 106);
+        // map.put("Vishal", 102);
+        // map.put("Kamale", 105);
+        // map.put("Kajal", 108);
+        // map.put("Rahul", 104);
+        System.out.println(map);
+        // System.out.println(map.ceilingEntry(107));
+        // System.out.println(map.ceilingKey(107));
+        // map.clear();
+        // System.out.println(map);
+        // System.out.println( map.containsKey(104));
+        // System.out.println(map.firstEntry());
+        // System.out.println(map.floorEntry(107));
+        // System.out.println(map.get(104));
+
+        // System.out.println(map.headMap(105));
+        // System.out.println(map.higherEntry(104));
+
+        // System.out.println(map.isEmpty());
+        // System.out.println(map.keySet());
+
+        // System.out.println(map.pollFirstEntry());
+        // System.out.println(map);
+
+        // System.out.println(map.pollLastEntry());
+        // System.out.println(map);
+
+        // System.out.println(map.remove(102));
+        // System.out.println(map);
+
+        // System.out.println(map.replace(102, "aaaa"));
+        // System.out.println(map);
+
+        // System.out.println(map.size());
+
+        // System.out.println(map.subMap(105, 106));
+
+    }
+}
