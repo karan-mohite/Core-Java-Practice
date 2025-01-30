67ec1f98aa3f72cd866649af34d3ff0fd1627e2e
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-30 09:09:57 +0530

    .

---
+++ Collections/Stack_Properties_Methods.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+public class Stack_Properties_Methods {
+    public static void main(String[] args) {
+        Stack s=new Stack<>();
+
+        s.push("Karan");
+        s.push("Vishal");
+        s.push(101);
+        s.push(10.99);
+        // System.out.println(s);
+        // System.out.println(s.pop());
+        // System.out.println(s);
+        // System.out.println(s.peek());
+        // System.out.println(s.search(101));
+
+        System.out.println(s.empty());  
+
+    }
+}
