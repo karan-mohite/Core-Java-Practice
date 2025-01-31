bd29f7aee81a2437387fbc838f74871e640d353f
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-01-31 13:21:52 +0530

    .

--- OOP Concepts/superKeywordDemo.java
+++ OOP Concepts/superKeywordDemo.java
@@ -1,16 +1,18 @@
-class superKeywordDemo
-{
-    int a=10;    
+class superKeywordDemo {
+    int a = 10;
 }
-class B extends superKeywordDemo
-{
-    int a=20;
-    void show(int a)
-    {
+
+class B extends superKeywordDemo {
+    int a = 20;
+
+    void show(int a) {
         System.out.println(a);
+        System.out.println(this.a);
+        System.out.println(super.a);
     }
+
     public static void main(String[] args) {
-        B ob=new B();
+        B ob = new B();
         ob.show(30);
     }
 }
