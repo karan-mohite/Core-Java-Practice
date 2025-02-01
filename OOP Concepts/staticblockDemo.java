b15960c69969dbfb092f9829704fafa08a140aaf
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-01 23:16:09 +0530

    .

--- OOP Concepts/staticblockDemo.java
+++ OOP Concepts/staticblockDemo.java
@@ -1,7 +1,11 @@
 public class staticblockDemo {
 
-    
+    static {
+        System.out.println("karan hello");
+        // System.exit(0);
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println("I am in main method");
     }
 }
