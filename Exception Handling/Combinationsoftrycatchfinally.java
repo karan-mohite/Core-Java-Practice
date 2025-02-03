e33e90e1bee8041f944901566ea5f240401b9e32
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-03 23:29:41 +0530

    .

---
+++ Exception Handling/Combinationsoftrycatchfinally.java
@@ -0,0 +1,34 @@
+public class Combinationsoftrycatchfinally {
+    public static void main(String[] args) {
+       try
+       {    
+            try{
+
+            }
+            catch(Exception e2)
+            {
+
+            }
+       }  
+       catch(Exception e)
+       {    
+             try{
+
+             }
+             catch(Exception e1)
+             {
+
+             }
+       }
+       finally
+       {
+            try{
+
+            }
+            catch(Exception e3)
+            {
+
+            }    
+       }
+    }
+}
