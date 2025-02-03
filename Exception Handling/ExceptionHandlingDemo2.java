4d487f622776faf7b87f04475438f819aae37ec6
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-03 18:36:43 +0530

    .

---
+++ Exception Handling/ExceptionHandlingDemo2.java
@@ -0,0 +1,15 @@
+public class ExceptionHandlingDemo2 {
+    public static void main(String[] args) {
+        try {
+            int a = 100, b = 0, c;
+            c = a / b;
+            System.out.println(c);
+        } catch (ArithmeticException e) {
+            // e.printStackTrace();
+            // System.out.println(e);
+            // System.out.println(e.toString());
+            System.out.println(e.getMessage());
+
+        }
+    }
+}
