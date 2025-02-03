e56ee8b78088aa21276d065682a35210286b694f
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-03 08:38:49 +0530

    .

--- Arrays/printArray.java
+++ Arrays/printArray.java
@@ -1,16 +1,15 @@
 public class printArray {
 
     public static void main(String[] args) {
-        int []a={10,20,30,40,50};
+        int[] a = { 10, 20, 30, 40, 50 };
 
         // for(int i=0;i<a.length;i++)
         // {
-        //     System.out.println(a[i]+" ");
+        // System.out.println(a[i]+" ");
         // }
 
-        for(int i:a)
-        {
-            System.out.println(i+" ");
+        for (int i : a) {
+            System.out.println(i + " ");
         }
 
     }
