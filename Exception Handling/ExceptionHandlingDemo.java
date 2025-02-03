855b5f3169895b6a2b6b8c00def96bed7ff2334c
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-03 18:26:04 +0530

    .

--- Exception Handling/ExceptionHandlingDemo.java
+++ Exception Handling/ExceptionHandlingDemo.java
@@ -3,15 +3,21 @@ import java.io.FileInputStream;
 public class ExceptionHandlingDemo {
 
     public static void main(String args[]) {
-        // FileInputStream fis=new FileInputStream("d:/abc.txt");
-        // Class.forName("com.mysql.jdbc.Driver");
 
-        // int a=100,b=0,c;
-        // c=a/b;
-        // System.out.println(c);
+        try {
+            // FileInputStream fis=new FileInputStream("d:/abc.txt");
+            // Class.forName("com.mysql.jdbc.Driver");
 
-        String name = null;
-        System.out.println(name.length());
+            int a = 100, b = 0, c;
+            c = a / b;
+            System.out.println(c);
 
+            // String name = null;
+            // System.out.println(name.length());
+
+        } catch (ArithmeticException e) {
+            System.out.println("You cannot divide by zero");
+        }
+        System.out.println("Hello");
     }
 }
