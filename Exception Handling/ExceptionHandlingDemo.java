d8369c25672284f34bc2c55d1a61920ca04178ad
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-03 18:14:41 +0530

    .

---
+++ Exception Handling/ExceptionHandlingDemo.java
@@ -0,0 +1,17 @@
+import java.io.FileInputStream;
+
+public class ExceptionHandlingDemo {
+
+    public static void main(String args[]) {
+        // FileInputStream fis=new FileInputStream("d:/abc.txt");
+        // Class.forName("com.mysql.jdbc.Driver");
+
+        // int a=100,b=0,c;
+        // c=a/b;
+        // System.out.println(c);
+
+        String name = null;
+        System.out.println(name.length());
+
+    }
+}
