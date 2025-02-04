34d67888526cfa2036711102dcf17b8001f59ece
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-04 11:49:15 +0530

    .

--- String in Java/TypeConversionMethod.java
+++ String in Java/TypeConversionMethod.java
@@ -8,11 +8,12 @@ public class TypeConversionMethod {
         // Type Conversion Method
         int a = 10, b = 20;
         System.out.println(a + b);
-        String s1 = String.valueOf(a);
+        String s1 = String.valueOf(a); 
         String s2 = String.valueOf(b);
         System.out.println(s1 + s2);
 
         char[] c = name.toCharArray();
         System.out.println(c);
+        System.out.println();
     }
 }
