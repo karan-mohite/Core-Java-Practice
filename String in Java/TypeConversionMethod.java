495ebebbcdb8eff0cf39806aa11f97c6635da2dd
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-04 11:45:41 +0530

    .

---
+++ String in Java/TypeConversionMethod.java
@@ -0,0 +1,18 @@
+public class TypeConversionMethod {
+    public static void main(String[] args) {
+        String name = "Deepak";
+        // Case Conversion Method
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+
+        // Type Conversion Method
+        int a = 10, b = 20;
+        System.out.println(a + b);
+        String s1 = String.valueOf(a);
+        String s2 = String.valueOf(b);
+        System.out.println(s1 + s2);
+
+        char[] c = name.toCharArray();
+        System.out.println(c);
+    }
+}
