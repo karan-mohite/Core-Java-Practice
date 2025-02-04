154c109eacf879d3f0daa67086869361f07b81d0
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-04 17:19:16 +0530

    .

---
+++ String in Java/StringBuilderDemo1.java
@@ -0,0 +1,10 @@
+public class StringBuilderDemo1 {
+    
+    public static void main(String[] args) {
+
+        StringBuilder sb=new StringBuilder("karan");
+        // System.out.println(sb.capacity());
+        // System.out.println(sb.length());
+        
+    }
+}
