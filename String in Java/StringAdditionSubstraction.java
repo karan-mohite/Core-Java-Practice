7fd95ec2bdedbf66f11181a8f891f1e1f8598ab9
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-04 11:00:31 +0530

    .

---
+++ String in Java/StringAdditionSubstraction.java
@@ -0,0 +1,30 @@
+public class StringAdditionSubstraction {
+    public static void main(String[] args) {
+        // String s1="karan";
+        // String s2="java";
+        
+        // System.out.println(s1+s2);
+        // System.out.println(s1+10);
+        // System.out.println(s1+10+20);
+        // System.out.println(10+s1+20);
+        // System.out.println(10+20+s1);
+        // System.out.println(s1+20/10);
+        // System.out.println(s1+10*20);
+        // // System.out.println(s1+10-20);
+        // System.out.println(s1.concat(s2));
+        // System.out.println(String.join(",",s1,s2,s1));
+
+        String s1="This is Demo";
+
+        System.out.println(s1.subSequence(3,8));
+        System.out.println(s1.substring(3));
+        System.out.println(s1.substring(3, 11));
+        System.out.println(s1.substring(0,0));
+    }
+}
+/*
+OUTPUT
+s is 
+s is Demo
+s is Dem
+ */
