847294766fe7b9ea325936593550a719936f4a57
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-04 18:33:00 +0530

    .

---
+++ String in Java/MaxRepChar.java
@@ -0,0 +1,28 @@
+public class MaxRepChar {
+    public static void main(String[] args) {
+        String s="this is demos abc xyz";
+        // s= s.replaceAll("\\s","");
+        // System.out.println(s);
+
+        int[] arr=new int[127];
+
+        for(int i=0;i<s.length();i++)
+        {
+            arr[s.charAt(i)]=arr[s.charAt(i)]+1;
+        }
+
+        int max=-1;
+        char c=' ';
+
+        for(int i=0;i<s.length();i++)
+        {
+            if(max<arr[s.charAt(i)])
+            {
+                max=arr[s.charAt(i)];
+                c=s.charAt(i);
+            }
+        }
+        System.out.println("Maximum Repeated Character is:"+c);
+
+    }
+}
