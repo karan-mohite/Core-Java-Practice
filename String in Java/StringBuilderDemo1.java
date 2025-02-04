ed76141fa8ccf6dc565b78469a221fbb3bd95a94
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-04 17:20:17 +0530

    .

--- String in Java/StringBuilderDemo1.java
+++ String in Java/StringBuilderDemo1.java
@@ -3,8 +3,7 @@ public class StringBuilderDemo1 {
     public static void main(String[] args) {
 
         StringBuilder sb=new StringBuilder("karan");
-        // System.out.println(sb.capacity());
-        // System.out.println(sb.length());
-        
+        System.out.println(sb.capacity());
+        System.out.println(sb.length());   
     }
 }
