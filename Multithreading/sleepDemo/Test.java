ef3c48b76ebeddcbe1a02fb314060729d476f720
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-05 12:22:25 +0530

    .

---
+++ Multithreading/sleepDemo/Test.java
@@ -0,0 +1,22 @@
+package sleepDemo;
+
+public class Test extends Thread {
+    public void run() {
+        try {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(i + ":" + Thread.currentThread().getName());
+                Thread.sleep(1000);
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public static void main(String[] args) {
+        Test t1 = new Test();
+        t1.start();
+
+        Test t2 = new Test();
+        t2.start();
+    }
+}
