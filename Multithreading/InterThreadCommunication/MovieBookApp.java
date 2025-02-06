b60dbbf20645d6116c3f8f3631e852f531f2136a
karan-mohite <142478056+karan-mohite@users.noreply.github.com>
2025-02-06 10:20:39 +0530

    .

--- Multithreading/InterThreadCommunication/MovieBookApp.java
+++ Multithreading/InterThreadCommunication/MovieBookApp.java
@@ -1,30 +1,27 @@
 package InterThreadCommunication;
 
-class TotalEarnings extends Thread
-{
-    int total=0;
-    public void run()
-    {
-        synchronized(this)
-        {
-            for(int i=1;i<=10;i++)
-            {
-                total=total+100;
+class TotalEarnings extends Thread {
+    int total = 0;
+
+    public void run() {
+        synchronized (this) {
+            for (int i = 1; i <= 10; i++) {
+                total = total + 100;
             }
             this.notify();
         }
     }
 }
+
 public class MovieBookApp {
-    public static void main(String[] args)throws InterruptedException {
-        TotalEarnings te=new TotalEarnings();
+    public static void main(String[] args) throws InterruptedException {
+        TotalEarnings te = new TotalEarnings();
         te.start();
         // System.out.println("Total Earngings:"+te.total+" RS");
 
-        synchronized(te)
-        {
+        synchronized (te) {
             te.wait();
-            System.out.println("Total Earnings:"+te.total+" Rs");
+            System.out.println("Total Earnings:" + te.total + " Rs");
         }
     }
 }
